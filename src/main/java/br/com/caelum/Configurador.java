0fc235eb2784e60a30d756afa0d839af5663498e
gustavo.tadei <gustavo.tadei@unir.br>
2020-02-19 09:22:50 -0400

    2 - Java e JPA Otimizações | 4 - O comportamento Lazy e OpenEntityManagerInView - OK, funcionando

--- src/main/java/br/com/caelum/Configurador.java
+++ src/main/java/br/com/caelum/Configurador.java
@@ -22,72 +22,84 @@ import br.com.caelum.dao.ProdutoDao;
 import br.com.caelum.model.Categoria;
 import br.com.caelum.model.Loja;
 import br.com.caelum.model.Produto;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("br.com.caelum")
 @EnableTransactionManagement
 public class Configurador extends WebMvcConfigurerAdapter {
-	
-	@Bean
-	@Scope("request")
-	public List<Produto> produtos(ProdutoDao produtoDao) {
-		List<Produto> produtos = produtoDao.getProdutos();
-		
-		return produtos;
-	}
-	
-	@Bean
-	public List<Categoria> categorias(CategoriaDao categoriaDao) { 
-		List<Categoria> categorias = categoriaDao.getCategorias();
-		
-		return categorias;
-	}
-	
-	@Bean
-	public List<Loja> lojas(LojaDao lojaDao) { 
-		List<Loja> lojas = lojaDao.getLojas();
-		
-		return lojas;
-	}
-	
-	@Bean
-	public MessageSource messageSource() { 
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		
-		messageSource.setBasename("/WEB-INF/messages");
-		messageSource.setCacheSeconds(1);
-		messageSource.setDefaultEncoding("ISO-8859-1");
-		
-		return messageSource;
-		
-	}
-	
-	@Bean 
-	public ViewResolver getViewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 
-		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-		
-		viewResolver.setExposeContextBeansAsAttributes(true);
+    @Bean
+    @Scope("request")
+    public List<Produto> produtos(ProdutoDao produtoDao) {
+        List<Produto> produtos = produtoDao.getProdutos();
 
-		return viewResolver;
-	}
+        return produtos;
+    }
+    
+    @Bean
+    public OpenEntityManagerInViewInterceptor getOpenEntityManagerInViewInterceptor(){
+        return new OpenEntityManagerInViewInterceptor();
+    }
 
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(new Converter<String, Categoria>() {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addWebRequestInterceptor(getOpenEntityManagerInViewInterceptor());
+    }
 
-			@Override
-			public Categoria convert(String categoriaId) {
-				Categoria categoria = new Categoria();
-				categoria.setId(Integer.valueOf(categoriaId));
-				
-				return categoria;
-			}
-			
-		});
-	}
-	
+    @Bean
+    public List<Categoria> categorias(CategoriaDao categoriaDao) {
+        List<Categoria> categorias = categoriaDao.getCategorias();
+
+        return categorias;
+    }
+
+    @Bean
+    public List<Loja> lojas(LojaDao lojaDao) {
+        List<Loja> lojas = lojaDao.getLojas();
+
+        return lojas;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("/WEB-INF/messages");
+        messageSource.setCacheSeconds(1);
+        messageSource.setDefaultEncoding("ISO-8859-1");
+
+        return messageSource;
+
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        viewResolver.setExposeContextBeansAsAttributes(true);
+
+        return viewResolver;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new Converter<String, Categoria>() {
+
+            @Override
+            public Categoria convert(String categoriaId) {
+                Categoria categoria = new Categoria();
+                categoria.setId(Integer.valueOf(categoriaId));
+
+                return categoria;
+            }
+
+        });
+    }
+
 }
